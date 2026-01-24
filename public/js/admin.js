085faa2ba0664a6fb763bb4fd389a10a37fff864
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2026-01-24 16:37:59 +0300

    add defult styling

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,24 @@
 function setPreviewContent(content) {
   const previewContent = document.getElementById('preview-content');
   const shadow = previewContent.shadowRoot || previewContent.attachShadow({ mode: 'open' });
-  shadow.innerHTML = `<style>:host{ background-color: white; } :host { all: initial; display: block; background-color: white; }</style>${content}`;
+  shadow.innerHTML = `<link rel="preload" href="/static/fonts/inter/InterVariable.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="/static/fonts/inter/InterVariable-Italic.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="stylesheet" href="/static/css/default.css">
+    <style>
+      :host {
+        all: initial;
+        display: block;
+        background-color: #F4F1EA;
+        color: #2D2A26;
+        font-family: 'Inter', system-ui, -apple-system, sans-serif;
+        font-size: 18px; 
+        line-height: 1.8;
+        padding: 2.5rem; 
+        margin: 0;
+        -webkit-font-smoothing: antialiased;
+        font-optical-sizing: auto;
+      }
+    </style>${content}`;
 }
 
 function updateSlug(value) {
